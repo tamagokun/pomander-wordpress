67ebc5f6721a2f0adbae08bdb83adcca64624128
Daniel Pakuschewski <contato@danielpk.com.br>
2013-09-28 15:04:08 -0300

    Changed how is created wp_config.php to allow wordpress use his own directory without public symlink inside. Need improve creation of init

--- lib/generators/config.php
+++ lib/generators/config.php
@@ -1,23 +1,28 @@
 <?php
 
-$env->repository('set your repository location here')
-    ->url('set your application url here')
-		->deploy_to('set your application location on server')
-		->releases(true)
-		//->user('set your ssh user')
-		//->scm('set your scm. defaults to git')
-		//->revision('')
-		//
-		//->backup(true)
-;
+/*
+	If you are at development environment leave these options below commented.
+	You should config only for staging or production environment.
+*/
 
-$env->app(array(             // Your application server(s) host or IP	
-	'your app-server here'
-));
+// $env->repository('set your repository location here');
+// $env->url('set your application url here');
+// $env->deploy_to('set your application location on server');
+// $env->releases(true);
+// $env->user('set your ssh user');
+// $env->scm('set your scm. defaults to git');
+// $env->revision('');
+// $env->backup(true);
 
-$env->db(array(              // Your database server(s) host or IP
-	'your db-server here'
-));
+// Config only for staging or production environment
+// $env->app(array(             // Your application server(s) host or IP	
+// 	'your app-server here'
+// ));
+
+// Config only for staging or production environment
+// $env->db(array(              // Your database server(s) host or IP
+// 	'your db-server here'
+// ));
 
 // If your app uses a database uncomment this:
 $env->database(array(
