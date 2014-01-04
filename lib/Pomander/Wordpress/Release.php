4aff6a2131683b0c996e6b0d67540a32f9a5caa5
Mike Kruk <tamagokun@gmail.com>
2014-01-04 17:15:56 -0500

    finished Release class

--- lib/Pomander/Wordpress/Release.php
+++ lib/Pomander/Wordpress/Release.php
@@ -16,24 +16,28 @@ class Release
 		$cmd = array();
 		$install_location = isset($env->wordpress["install_dir"]) ? $env->wordpress["install_dir"] : "wordpress";
 		$root_install = empty($install_location);
-		$install_dir = $env->release_dir."/".rtrim("/", $install_location);
+		$install_dir = $env->release_dir."/".rtrim($install_location, "/");
 
 		// make sure install location exists
 		$cmd[] = "mkdir -p $install_dir";
 		// download release
-		$cmd[] = "curl {$this->release_url()} > {$env->release_dir}/wordpress.tar";
+		$cmd[] = "curl -s {$this->release_url()} > {$env->release_dir}/wordpress.tar";
 		// extract
 		if($root_install)
 		{
 			$cmd[] = "tar --strip-components=1 -xzf {$env->release_dir}/wordpress.tar";
 		}else
 		{
-			$cmd[] = "tar --strip-components=1 -xzf {$env->release_dir}/wordpress.tar -C $install_dir";
+			$cmd[] = "mkdir -p {$install_dir}";
+			$cmd[] = "tar --strip-components=1 -xzf {$env->release_dir}/wordpress.tar -C {$install_dir}";
 			$cmd[] = "rm -rf $install_dir/public";
 			$cmd[] = "ln -s {$env->release_dir}/public $install_dir/public";
 			$cmd[] = "rm -rf $install_dir/vendor";
 			$cmd[] = "ln -s {$env->release_dir}/vendor $install_dir/vendor";
 		}
+
+		// cleanup
+		$cmd[] = "rm -rf {$env->release_dir}/wordpress.tar";
 
 		// make sure plugin dir and .htaccess exist
 		$cmd[] = "mkdir -p {$env->release_dir}/vendor/plugins";
@@ -44,7 +48,7 @@ class Release
 	public function latest()
 	{
 		$data = json_decode(file_get_contents("http://api.wordpress.org/core/version-check/1.7/"));
-		return $data["offers"][0]["current"];
+		return $data->offers[0]->current;
 	}
 
 	public function release_url()
